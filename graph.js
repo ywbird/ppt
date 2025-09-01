793b6237331aec7d9b83d8801c6c0538a8885b45
ywbird <ywbird@naver.com>
2025-09-02 03:10:00 +0900

    gab horn inc UV

--- graph.js
+++ graph.js
@@ -1,9 +1,10 @@
 /// <reference path="https://jsxgraph.org/distrib/index.d.ts" />
 
 let board = JXG.JSXGraph.initBoard("box", {
-	boundingbox: [-4, 4, 4, -4],
+	boundingbox: [-2 * Math.PI, 2, 2 * Math.PI, -2],
 	axis: true,
 	copyright: false,
 });
 
-board.create("functiongraph", [(x) => 1 / x], { strokeWidth: 4 });
+let f = board.jc.snippet("cos(x)*cos(x)", true, "x", true);
+board.create("functiongraph", [f], { strokeWidth: 4 });
